f0355bfc59fc30f823db13fd2b8b60d62c6613cf
AmitTzah <93420076+AmitTzah@users.noreply.github.com>
2021-12-17 11:39:22 +0200

    Cofigured Firebase- added needed packages. added both ios and android apps to firebase project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:badge_ai/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+//firebase:
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 void main() {
   runApp(const MyApp());
