1c6ce3abe54ed6558743be16fc5cd88f33838f94
AmitTzah <93420076+AmitTzah@users.noreply.github.com>
2022-01-16 15:27:58 +0200

    Login screen should now be responsive.

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -2,6 +2,7 @@ import 'package:badge_ai/routes/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:sizer/sizer.dart';
 import '../button.dart';
 import '../constants.dart';
 import 'home.dart';
@@ -31,137 +32,128 @@ class _LoginScreenState extends State<LoginScreen> {
               key: formkey,
               child: AnnotatedRegion<SystemUiOverlayStyle>(
                 value: SystemUiOverlayStyle.light,
-                child: Stack(
-                  children: [
-                    Container(
-                      height: double.infinity,
-                      width: double.infinity,
-                      color: Colors.grey[200],
-                      child: SingleChildScrollView(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 25, vertical: 120),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Text(
-                              "Sign In",
-                              style: TextStyle(
-                                  fontSize: 50,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            const SizedBox(height: 30),
-                            TextFormField(
-                              keyboardType: TextInputType.emailAddress,
-                              onChanged: (value) {
-                                email = value;
-                              },
-                              validator: (value) {
-                                if (value!.isEmpty) {
-                                  return "Please enter Email";
-                                }
-                              },
-                              textAlign: TextAlign.center,
-                              decoration: kTextFieldDecoration.copyWith(
-                                hintText: 'Email',
-                                prefixIcon: const Icon(
-                                  Icons.email,
-                                  color: Colors.black,
+                child: SingleChildScrollView(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 4.w, vertical: 10.h),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Sign In",
+                        style: TextStyle(
+                            fontSize: 40.sp,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 5.h),
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        onChanged: (value) {
+                          email = value;
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Please enter Email";
+                          }
+                        },
+                        textAlign: TextAlign.center,
+                        decoration: kTextFieldDecoration.copyWith(
+                          hintText: 'Email',
+                          prefixIcon: const Icon(
+                            Icons.email,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 5.h),
+                      TextFormField(
+                        obscureText: true,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Please enter Password";
+                          }
+                        },
+                        onChanged: (value) {
+                          password = value;
+                        },
+                        textAlign: TextAlign.center,
+                        decoration: kTextFieldDecoration.copyWith(
+                            hintText: 'Password',
+                            prefixIcon: const Icon(
+                              Icons.lock,
+                              color: Colors.black,
+                            )),
+                      ),
+                      SizedBox(height: 15.h),
+                      LoginSignupButton(
+                        title: 'Login',
+                        ontapp: () async {
+                          if (formkey.currentState!.validate()) {
+                            setState(() {
+                              isloading = true;
+                            });
+                            try {
+                              await _auth.signInWithEmailAndPassword(
+                                  email: email, password: password);
+                              await Navigator.of(context)
+                                  .pushNamed(RouteManager.homePage);
+                              setState(() {
+                                isloading = false;
+                              });
+                            } on FirebaseAuthException catch (e) {
+                              showDialog(
+                                context: context,
+                                builder: (ctx) => AlertDialog(
+                                  title: const Text("Ops! Login Failed"),
+                                  content: Text('${e.message}'),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.of(ctx).pop();
+                                      },
+                                      child: const Text('Okay'),
+                                    )
+                                  ],
                                 ),
-                              ),
+                              );
+                              // ignore: avoid_print
+                              print(e);
+                            }
+                            setState(() {
+                              isloading = false;
+                            });
+                          }
+                        },
+                      ),
+                      SizedBox(height: 1.h),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context)
+                              .pushNamed(RouteManager.register);
+                        },
+                        child: Row(
+                          children: [
+                            Text(
+                              "Don't have an Account ?",
+                              style: TextStyle(
+                                  fontSize: 15.sp, color: Colors.black87),
                             ),
-                            const SizedBox(height: 30),
-                            TextFormField(
-                              obscureText: true,
-                              validator: (value) {
-                                if (value!.isEmpty) {
-                                  return "Please enter Password";
-                                }
-                              },
-                              onChanged: (value) {
-                                password = value;
-                              },
-                              textAlign: TextAlign.center,
-                              decoration: kTextFieldDecoration.copyWith(
-                                  hintText: 'Password',
-                                  prefixIcon: const Icon(
-                                    Icons.lock,
-                                    color: Colors.black,
-                                  )),
-                            ),
-                            const SizedBox(height: 80),
-                            LoginSignupButton(
-                              title: 'Login',
-                              ontapp: () async {
-                                if (formkey.currentState!.validate()) {
-                                  setState(() {
-                                    isloading = true;
-                                  });
-                                  try {
-                                    await _auth.signInWithEmailAndPassword(
-                                        email: email, password: password);
-                                    await Navigator.of(context)
-                                        .pushNamed(RouteManager.homePage);
-                                    setState(() {
-                                      isloading = false;
-                                    });
-                                  } on FirebaseAuthException catch (e) {
-                                    showDialog(
-                                      context: context,
-                                      builder: (ctx) => AlertDialog(
-                                        title: const Text("Ops! Login Failed"),
-                                        content: Text('${e.message}'),
-                                        actions: [
-                                          TextButton(
-                                            onPressed: () {
-                                              Navigator.of(ctx).pop();
-                                            },
-                                            child: const Text('Okay'),
-                                          )
-                                        ],
-                                      ),
-                                    );
-                                    // ignore: avoid_print
-                                    print(e);
-                                  }
-                                  setState(() {
-                                    isloading = false;
-                                  });
-                                }
-                              },
-                            ),
-                            const SizedBox(height: 10),
-                            GestureDetector(
-                              onTap: () {
-                                Navigator.of(context)
-                                    .pushNamed(RouteManager.register);
-                              },
-                              child: Row(
-                                children: const [
-                                  Text(
-                                    "Don't have an Account ?",
-                                    style: TextStyle(
-                                        fontSize: 20, color: Colors.black87),
-                                  ),
-                                  SizedBox(width: 10),
-                                  Hero(
-                                    tag: '1',
-                                    child: Text(
-                                      'Sign up',
-                                      style: TextStyle(
-                                          fontSize: 21,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.black),
-                                    ),
-                                  )
-                                ],
+                            SizedBox(width: 10),
+                            Hero(
+                              tag: '1',
+                              child: Text(
+                                'Sign up',
+                                style: TextStyle(
+                                    fontSize: 19.sp,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
                               ),
                             )
                           ],
                         ),
-                      ),
-                    )
-                  ],
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
